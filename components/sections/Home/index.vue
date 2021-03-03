ea2f0ec6c7f38f01d119f5651c2ff3225f0e189d
Julio Alberto <julio.aranis.contreras@gmail.com>
2021-03-03 20:52:14 +0100

    fix mobile screen

--- components/sections/Home/index.vue
+++ components/sections/Home/index.vue
@@ -2,13 +2,13 @@
   <div class="grid grid-r3 h-screen">
     <div />
     <div class="w-full flex flex-col ">
-      <div class="pt-64 lg:pt-132 flex justify-center content-center font-bold flex-wrap items-center text-lg">
+      <div class="pt-48 lg:pt-132 flex justify-center content-center font-bold flex-wrap items-center text-lg">
         <p class="text-center leading-normal text-2xl sm:text-4xl lg:text-5xl text-primary uppercase w-full lg:w-7/12">
           <pre class="title text-glow" />
         </p>
       </div>
     </div>
-    <div class="flex w-full justify-center text-secondary items-center uppercase pb-12 sm:pb-0">
+    <div class="flex w-full justify-center text-secondary items-center uppercase pb-20 sm:pb-0">
       <a class="font  lg:text-secondary link text-base lg:text-xl cursor-pointer" @click="about">
         About
       </a>
